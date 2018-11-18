b384c9357e67b93357bfc8192f3510476c32decd
Jay Fox <j.hartusch@gmx.de>
2018-11-18 22:19:00 +0100

    Repaired fetchArticles function and refactored a bit

--- dgapCrawler.js
+++ dgapCrawler.js
@@ -14,7 +14,7 @@ let crawl = async () => {
 
     while (keepGoing) {
         await page.goto(config.baseUrl);
-        let data = await fetchData(page);
+        let data = await fetchCompanys(page);
 
         // ------- get titles ---------------
         for (dataIndex = 0; dataIndex < data.companies.length; dataIndex++) {
@@ -76,27 +76,20 @@ let crawl = async () => {
         firstSite = false;
     } // ----- END WHILE
 
-    var newsJson = JSON.stringify(news[0]);
-
-    var fs = require('fs');
-    fs.writeFile("news.json", newsJson, function (err) {
-        if (err) throw err;
-        console.log('Saved!');
-    });
-
+    writeNewsToFile(news)
     browser.close();
 };
 
-fetchData = async function(page) {
-    console.log("Start fetch articles...");
+fetchCompanys = async function(page) {
+    console.log("Start fetching articles...");
     return await page.evaluate(() => {
-        debugger;
-        const companies = []; 
-        var time = new Date().toLocaleDateString("de-DE");
-        const articleTableBody = document.querySelector("#content div.content_list.news_list table > tbody");
-        let articles = articleTableBody.querySelectorAll("tr");
-        
-        for (let articleIndex = 1; articleIndex <= articles.length; articleIndex++) {
+        // function declaration -  functions cannot be passed to page.evaluate without hack https://stackoverflow.com/a/52176714
+        const isNews = function(infoParts) {
+            return infoParts[1].includes("DGAP-News");
+        }
+
+        const getCompanyWithValues = function(infoParts, linkElement) {
+            var today = new Date().toLocaleDateString("de-DE", { day:"2-digit", month:"2-digit", year:"2-digit" });
             let company = {
                 name: "",
                 title: "",
@@ -105,37 +98,52 @@ fetchData = async function(page) {
                 link: "",
                 stop: false
             }
-            
-            let link = articleTableBody.querySelector("tr:nth-child(" + articleIndex + ") > td.content_text > a")
-            let infos = link.querySelector("strong")
-            
-            if (infos == null) {
-                continue
-            }
-            let info = infos.innerText.split(" ");
-            
-            if (!info[1].includes("DGAP-News")) {
-                continue
-            }
-            company.date = info[0]
-            if (info[0] != time) {
+            company.date = infoParts[0];
+            if (infoParts[0] != today) {
                 company.stop = true;
-                companies.push(company)
-                break
+                return company; 
             }
-            company.time = info[1].substring(0, 5);
-            company.name = info[2]
-            for (infoIndex = 3; infoIndex < info.length; infoIndex++) {
-                company.name = company.name + " " + info[infoIndex]
+            company.time = infoParts[1].substring(0, 5);
+            company.name = infoParts[2];
+            for (let infoIndex = 3; infoIndex < infoParts.length; infoIndex++) {
+                company.name = company.name + " " + infoParts[infoIndex];
             }
-            if (link != null) {
-                company.link = link.href;
+            if (linkElement != null) {
+                company.link = linkElement.href;
             }
-            companies.push(company);
+            return company
+        }
+
+        debugger;
+        const companies = []; 
+        const articleTableBody = document.querySelector("#content div.content_list.news_list table > tbody");
+        let articles = articleTableBody.querySelectorAll("tr");
+        
+        for (let articleIndex = 1; articleIndex <= articles.length; articleIndex++) {
+            let linkElement = articleTableBody.querySelector("tr:nth-child(" + articleIndex + ") > td.content_text > a")
+            let infosElement = linkElement.querySelector("strong")
+            
+            if (infosElement != null) {
+                let infoParts = infosElement.innerText.split(" ");
+                if (isNews(infoParts)) {
+                    let company = getCompanyWithValues(infoParts, linkElement)
+                    companies.push(company);
+                    if(company.stop) break;
+                }
+            }
         }
         return { companies };
     });
 }
 
-crawl()
+writeNewsToFile = function(news) {
+    var newsJson = JSON.stringify(news[0]);
 
+    var fs = require('fs');
+    fs.writeFile("news.json", newsJson, function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+    });
+}
+
+crawl();
