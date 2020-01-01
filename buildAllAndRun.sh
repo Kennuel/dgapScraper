4b61058934f116bb00e848e60e25e2ed305a8935
Kennuel <>
2020-01-01 13:44:06 +0100

    Added Missing then

--- buildAllAndRun.sh
+++ buildAllAndRun.sh
@@ -1,44 +1,50 @@
 ﻿docker-compose down
 
-if ["$0" == "articleprocessor"] || ["$0" == ""] 
-    cd .\ArticleProcessor
-    mvn clean install
-    docker build -t articleprocessor .
-    cd ..
+if ["$0" == "articleprocessor"] || ["$0" == ""]
+    then
+        cd .\ArticleProcessor
+        mvn clean install
+        docker build -t articleprocessor .
+        cd ..
 fi
 
 if ["$0" == "persistence"] || ["$0" == ""] 
-    cd .\database
-    docker build -t database .
-    cd ..
+    then
+        cd .\database
+        docker build -t database .
+        cd ..
 fi
 
 if ["$0" == "stockProcessor"] || ["$0" == ""] 
-    cd .\StockProcessor
-    mvn clean install
-    docker build -t stockprocessor .
-    cd ..
+    then
+        cd .\StockProcessor
+        mvn clean install
+        docker build -t stockprocessor .
+        cd ..
 fi
 
 if ["$0" == "stockProcessor"] || ["$0" == ""] 
-    cd .\StockProcessor
-    mvn clean install
-    docker build -t stockprocessor .
-    cd ..
+    then
+        cd .\StockProcessor
+        mvn clean install
+        docker build -t stockprocessor .
+        cd ..
 fi
 
 if ["$0" == "singleScraper"] || ["$0" == ""] 
-    cd .\singleScraper
-    mvn clean install
-    docker build -t singlescraper .
-    cd ..
+    then
+        cd .\singleScraper
+        mvn clean install
+        docker build -t singlescraper .
+        cd ..
 fi
 
 if ["$0" == "stockScraper"] || ["$0" == ""] 
-    cd .\stockScraper
-    mvn clean install
-    docker build -t stockscraper .
-    cd ..
+    then
+        cd .\stockScraper
+        mvn clean install
+        docker build -t stockscraper .
+        cd ..
 fi
 
 docker-compose up
