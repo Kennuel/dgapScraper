950803b61190f9f0b97a23af442bcd0ee803c6bd
Kennuel <>
2020-01-01 13:23:31 +0100

    Added bash script für Linux

---
+++ buildAllAndRun.sh
@@ -0,0 +1,44 @@
+﻿docker-compose down
+
+if ["$0" == "articleprocessor"] || ["$0" == ""] 
+    cd .\ArticleProcessor
+    mvn clean install
+    docker build -t articleprocessor .
+    cd ..
+fi
+
+if ["$0" == "persistence"] || ["$0" == ""] 
+    cd .\database
+    docker build -t database .
+    cd ..
+fi
+
+if ["$0" == "stockProcessor"] || ["$0" == ""] 
+    cd .\StockProcessor
+    mvn clean install
+    docker build -t stockprocessor .
+    cd ..
+fi
+
+if ["$0" == "stockProcessor"] || ["$0" == ""] 
+    cd .\StockProcessor
+    mvn clean install
+    docker build -t stockprocessor .
+    cd ..
+fi
+
+if ["$0" == "singleScraper"] || ["$0" == ""] 
+    cd .\singleScraper
+    mvn clean install
+    docker build -t singlescraper .
+    cd ..
+fi
+
+if ["$0" == "stockScraper"] || ["$0" == ""] 
+    cd .\stockScraper
+    mvn clean install
+    docker build -t stockscraper .
+    cd ..
+fi
+
+docker-compose up
